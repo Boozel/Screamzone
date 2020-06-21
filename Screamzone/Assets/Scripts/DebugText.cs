c3b1d2f4a13d1628f7fe6b0606dc5fcde921b012
boozel <gerrittdorland@gmail.com>
2020-06-21 14:16:59 -0700

    I think I missed some stuff due to Unity being open

---
+++ Screamzone/Assets/Scripts/DebugText.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DebugText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public TextMeshPro textMesh;
+    void Start()
+    {
+        textMesh = (TextMeshPro)GameObject.Find ("DebugText").GetComponent<TextMeshPro>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textMesh.SetText("Volume: {0:3}", MicInput.MicLoudness);
+    }
+}
